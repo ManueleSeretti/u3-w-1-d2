768af71cf5c6f94abe4b509972b6f904b8c23408
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-09-05 16:24:23 +0200

    ...

--- src/component/AllTheBooks.jsx
+++ src/component/AllTheBooks.jsx
@@ -5,13 +5,18 @@ class AllTheBooks extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row row-cols-lg-4 row-cols-xl-5 gy-3">
+        <div className="row row-cols-md-4 row-cols-lg-5 row-cols-xl-7 gy-3">
           {books.map((book) => (
             <div key={book.asin} className="col">
               <div className="card">
-                <img src={book.img} className="card-img-fluid" style={{ objectFit: "cover" }} alt="..." />
+                <img
+                  src={book.img}
+                  className="card-img-fluid"
+                  style={{ height: "350px", objectFit: "cover" }}
+                  alt="..."
+                />
                 <div className="card-body">
-                  <h5 className="card-title">{book.title}</h5>
+                  <h5 className="card-title text-truncate">{book.title}</h5>
                   <p className="card-text">$ {book.price} </p>
                 </div>
               </div>
